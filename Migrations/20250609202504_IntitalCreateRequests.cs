ad9e366554cbeaae7b50f32ba45b3040054f7271
Dave Neve <64417697+nevosnr@users.noreply.github.com>
2025-06-09 21:41:29 +0100

    Created MVC CRUD operation on Requests and tested.

--- Migrations/20250609202504_IntitalCreateRequests.cs
+++ Migrations/20250609202504_IntitalCreateRequests.cs
@@ -8,6 +8,8 @@ namespace FinalProjectAl1.Migrations
     /// <inheritdoc />
     public partial class IntitalCreateRequests : Migration
     {
+        // Cleared out the migration file as per https://stackoverflow.com/questions/53210060/what-is-the-equivalent-of-the-ignorechanges-switch-for-entity-framework-core-in
+        // where the update to EF Core removed the -IgnoreChanges flag
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
