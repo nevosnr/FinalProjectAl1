782e8a0de786ab8a6fbc29fb92cdbb2978f7e50b
Dave Neve <64417697+nevosnr@users.noreply.github.com>
2025-06-28 22:37:46 +0100

    we'll see if this works moving to Blazor!!

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
+﻿using FinalProjectAl1.Components;
 using FinalProjectAl1.Data;
 using FinalProjectAl1.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using MudBlazor.Services;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddMudServices();
 // Add services to the container.
 
 //Connection using the default connection in appsettings.json (used for identity framework)
@@ -17,14 +22,19 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
 
 });
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
 //New connection variable for the main Db connection.
 var tmscconnectionstring = builder.Configuration.GetConnectionString("TMSCFrontDoor") ?? throw new InvalidOperationException("Connection string 'TMSCFrontDoor' not found.");
 //Adding correct context for Request functions.
-builder.Services.AddDbContext<RequestDbContext>(options =>
+builder.Services.AddDbContextFactory<RequestDbContext>(options =>
 {
     options.UseSqlServer(tmscconnectionstring);
 
 });
+
+
+builder.Services.AddCascadingAuthenticationState();
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
@@ -35,7 +45,13 @@ builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
+//builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
@@ -49,21 +65,26 @@ else
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseAntiforgery();
 
 app.MapStaticAssets();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}")
+//    .WithStaticAssets();
 
-app.MapRazorPages()
-   .WithStaticAssets();
+//app.MapRazorPages()
+//   .WithStaticAssets();
+
+app.MapRazorComponents<App>();
 
 app.Run();
